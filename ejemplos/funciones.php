84fa16ec238ec738c2db6fd07b219348ea148c00
foap <foap@BI101-08.epsevg.upc.es>
2019-05-06 10:20:49 +0200

    arreglo error

---
+++ ejemplos/funciones.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/favicon.ico" type="img/x-icon" />
+    <title>Ejemplo funciones</title>
+    <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+</head>
+
+<body>
+    <noscript>
+        <p>Para visualizar correctamente esta página, deberá habilitar el uso de JavaScript.</p>
+    </noscript>
+
+    <?php
+    $numeros=generaarray(10);
+    print_r($numeros);
+    ?>
+
+
+
+</body>
+</html>
