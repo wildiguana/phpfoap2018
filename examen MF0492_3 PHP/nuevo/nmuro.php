caaab05e141408fc8f7716d0bd386ebea99ecb19
Jose <not2hoax@gmail.com>
2019-05-10 17:41:50 +0200

    ctrl logout

--- examen MF0492_3 PHP/nuevo/nmuro.php
+++ examen MF0492_3 PHP/nuevo/nmuro.php
@@ -5,16 +5,16 @@ if(isset($_REQUEST["logout"])){
     session_destroy(); 
     setcookie("password",0,1);
     setcookie("nomusuari",0,1);
-    header('Location:ejemplo_login.php');           
+    header('Location:nportada.php');           
 }
 if(isset($_SESSION["login"])&&$_SESSION["login"]==true){
 ?>
 Bienvenido......<?=$_SESSION["nom"]?>
 
-<a href="ejemplo_privado.php?logout">[logout]</a>
+<a href="nmuro.php?logout">[logout]</a>
 <?php
 }else{
-    header('Location:ejemplo_login.php');           
+    header('Location:nportada.php');           
 }
 ?>
 
