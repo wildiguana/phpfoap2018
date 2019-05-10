a440a566b91cd9481a5deb8b77cd706422d8da9e
Jose <not2hoax@gmail.com>
2019-05-10 17:52:20 +0200

    mm

--- examen MF0492_3 PHP/nuevo/nmuro.php
+++ examen MF0492_3 PHP/nuevo/nmuro.php
@@ -14,7 +14,7 @@ Bienvenido......<?=$_SESSION["nom"]?>
 <a href="nportada.php?logout">[logout]</a>
 <?php
 }else{
-    header('Location:nportada.php');           
+    header('Location:nmuro.php');           
 }
 ?>*/
 
