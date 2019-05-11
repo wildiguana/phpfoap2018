6ee587daad7cb2c4b8db64fd1bfcaf2829ec7d74
Jose <not2hoax@gmail.com>
2019-05-11 15:10:13 +0200

    tst

--- examen MF0492_3 PHP/test privado/test_publica.php
+++ examen MF0492_3 PHP/test privado/test_publica.php
@@ -2,7 +2,7 @@
 session_start();
 $error="";
 if(isset($_SESSION["login"])){
-    header('Location:test_publica.php');           
+    header('Location:test_privado.php');           
 }
 /*if(isset($_COOKIE["password"])){
     if($_COOKIE["password"]==1234){
