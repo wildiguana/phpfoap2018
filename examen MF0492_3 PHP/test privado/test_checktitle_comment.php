e7fa117019944f34f5f7a582b98dacdf1f4f3453
Jose <not2hoax@gmail.com>
2019-05-12 13:27:19 +0200

    str

--- examen MF0492_3 PHP/test privado/test_checktitle_comment.php
+++ examen MF0492_3 PHP/test privado/test_checktitle_comment.php
@@ -1,14 +1,19 @@
 <?php
         $titulo = $comentarios = "";
-        $tituloErr = $comentariosErr="";
+        $tituloErr = $comentariosErr = "";
         if (isset($_REQUEST['submit'])){
             if (empty($_REQUEST["titulo"])) {
-                $tituloErr = "Debes introducir un título, recuerda mínimo 10 caracteres";
+                $tituloErr = "Debes introducir un título.";
             }else{
                 $titulo = test_input($_REQUEST["titulo"]);
+                    $str ='titulo';
+                    echo $str = '$str';
+                //if (!preg_match ($str = "titulo")=>10)) {
+                   // $tituloErr = "mínimo 10 caracteres."; 
+                  //}
             }
             if (empty($_REQUEST["comentarios"])) {
-                $comentariosErr = "Debes introducir un comentario, recuerda máximo 500 caracteres";
+                $comentariosErr = "Debes introducir un comentario, recuerda máximo 500 caracteres.";
             }else{
                 $comentarios = test_input($_REQUEST["comentarios"]);
             }
@@ -20,13 +25,13 @@
             return $data;
         }
     ?>
-            <form  method="post" action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>"> 
+            <form  method="post" action="<?php=htmlspecialchars($_SERVER["PHP_SELF"])?>"> 
                 <input type="text" name="titulo" value="<?=$titulo?>">
-                    <span class="error">* <?=$tituloErr?></span>
+                    <span class="error">* <?php=$tituloErr?></span>
                     <br><br>
                 <textarea name="comentarios" rows="5" cols="40" value="<?=$comentarios?>">
-                </textarea>
-                <span class="error">* <?=$comentariosErr?></span>
-                <br><br>
+                    </textarea>
+                    <span class="error">* <?php=$comentariosErr?></span>
+                    <br><br>
                 <input type="submit" name="submit" value="aceptar">
             </form>
