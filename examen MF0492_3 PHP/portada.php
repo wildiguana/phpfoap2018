9e0625df50d18ad30bd520d93ae5302a77f934ab
Jose <not2hoax@gmail.com>
2019-05-12 18:43:27 +0200

    foot

--- examen MF0492_3 PHP/portada.php
+++ examen MF0492_3 PHP/portada.php
@@ -128,13 +128,13 @@
                 <div class="container">
                   <?php
                   echo
-                  <p class="m-0 text-center text-white">AVISO LEGAL:
+                  "<p>AVISO LEGAL:<br>
                     De acuerdo con lo que establece la Ley Orgánica de Protección de Datos 
                     (LOPD) 15/1999, le informamos que los datos personales recogidos en este 
                     formulario serán incluidos en un fichero bajo la responsabilidad de 
                     YOURSELF, S.L.. Puede ejercer sus derechos de acceso, rectificación, 
                     cancelación y oposición en: YOURSELF, S.L., Calle NS/NC Nº 007, 08000 – JAEN. 
-                    Your Website 2019</p>
+                    Your Website 2019</p>";
                     ?>
                 </div>
                 </footer>
