c3dee5147699c6a8d7c99df6db34bb64e2084995
foap <foap@BI101-08.epsevg.upc.es>
2019-05-20 13:35:15 +0200

    test tabla platos

---
+++ mysql/ejemplo platos.sql
@@ -0,0 +1,32 @@
+
+CREATE DATABASE IF NOT EXISTS restaurant;
+
+
+use restaurant;
+
+CREATE TABLE IF NOT EXISTS platos1(
+cod_plato CHAR(4) PRIMARY KEY,
+nom VARCHAR(30) NOT NULL,
+tipo VARCHAR(30)
+);
+
+
+CREATE TABLE IF NOT EXISTS vinos1(
+cod_vino CHAR(4) PRIMARY KEY,
+nom VARCHAR(30) NOT NULL,
+precio FLOAT(10),
+distribuidor CHAR(4),
+FOREIGN KEY (distribuidor) REFERENCES distribuidor1 (cod_dist)
+ON UPDATE CASCADE
+ON DELETE RESTRICT
+);
+
+
+CREATE TABLE IF NOT EXISTS distribuidor1(
+cod_dist CHAR(4) PRIMARY KEY,
+nom VARCHAR(30) NOT NULL,
+tel INT(9)
+);
+
+
+
