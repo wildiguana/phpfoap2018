0540a740748546413a0af2ddeff693b5ea3c08b8
foap <foap@BI101-08.epsevg.upc.es>
2019-06-10 11:14:08 +0200

    none

--- prueba crud/crud_prueba.php
+++ prueba crud/crud_prueba.php
@@ -30,7 +30,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header clearfix">
-                        <h2 class="pull-left">Employees Details</h2>
+                        <h2 class="pull-left">Usuarios</h2>
                         <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
                     </div>
                     <?php
