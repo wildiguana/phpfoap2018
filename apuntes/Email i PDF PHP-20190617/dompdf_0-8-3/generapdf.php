cb0b1acd7d8a8cb2b699eddc80130f9d5dfff378
foap <foap@BI101-08.epsevg.upc.es>
2019-06-17 13:57:46 +0200

    imprimir pdf

---
+++ apuntes/Email i PDF PHP-20190617/dompdf_0-8-3/generapdf.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'dompdf/autoload.inc.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello w_o_r_l_d');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+
+//Setting Options
+//Set options are not required
+
+?>
