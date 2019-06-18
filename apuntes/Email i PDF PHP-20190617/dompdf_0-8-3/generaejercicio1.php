daa4a263d0c8822e507e2617ebd624c10947c198
foap <foap@BI101-08.epsevg.upc.es>
2019-06-18 12:12:21 +0200

    none

---
+++ apuntes/Email i PDF PHP-20190617/dompdf_0-8-3/generaejercicio1.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'dompdf/autoload.inc.php';
+require ("ejercicio1_lista_clientes.php");
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello w_o_r_l_d');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+
+//Setting Options
+//Set options are not required
+
+?>
