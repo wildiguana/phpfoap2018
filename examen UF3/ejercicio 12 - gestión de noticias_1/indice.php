21e17df9836d86ca4d2818a7c2a69edcddff88ec
Jose <not2hoax@gmail.com>
2019-06-20 19:52:29 +0200

    gestion_1

---
+++ examen UF3/ejercicio 12 - gestión de noticias_1/indice.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Examen UF 3 - Jose Dalcolmo - portada</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="index ej 12">
+    <meta name="author" content="joseO">
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#"><h2><u>Coffee News</u></h2></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+          <li class="nav-item">
+            <a class="nav-link" href="https://cnnespanol.cnn.com/?hpt=header_edition-picker"
+            target="_blank">Salir</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content--> 
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+      <h1 class="mt-5">Gestión de noticias</h1>
+        <h4 class="mt-5"><u>Accerso usuarios registrados</u></h4>
+</div>
+        <ul class="list-unstyled">
+        
+          <form method="post">
+
+          <div class="form-group">
+  <label for="usr"><h4>Usuario:</h4></label>
+  <input type="text" class="form-control" id="">
+</div>
+<div class="form-group">
+  <label for="pwd"><h4>Contraseña:</h4></label>
+  <input type="password" class="form-control" id="">
+  <br>
+  <input type="submit" class="btn btn-outline-success" name="submit" value="Enviar">
+</div>
+        <li><h4>No está registrado?</h4></li>
+        <a class="nav-link" href="#registro.php" target="_blank"><button type="button" 
+        class="btn btn-outline-dark">Regístrese aquí</button></a>
+
+        </ul>
+      </div>
+    
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+       
+          
+        
+<?php
+
+
+?>
+</body>
+</html>
---
+++ examen UF3/ejercicio 12 - gestión de noticias_1/menu.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Examen UF 3 - Jose Dalcolmo - menu</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="index ej 12">
+    <meta name="author" content="joseO">
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#"><h2><u>Coffee News</u></h2></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><button type="button" class="btn btn-default btn-primary">Noticias: Consultar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><button type="button" class="btn btn-default btn-success">Insertar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><button type="button" class="btn btn-default btn-danger">Borrar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><button type="button" class="btn btn-default btn-warning">Cerrar sesión</a>
+        </button></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <h1 class="mt-5">Gestión de noticias</h1>
+        <p class="lead">Página</p>
+        <ul class="list-unstyled">
+          <li>Inicio</li>
+          
+       
+        <button type="button" class="btn btn-outline-dark">Enviar</button>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="jquery/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+       
+          
+        
+<?php
+
+
+?>
+</body>
+</html>
